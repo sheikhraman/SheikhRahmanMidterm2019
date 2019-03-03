d03bc58768732f05f12f9264d11cbb8149cae90f
sheikhraman <sheikhraman@yahoo.com>
2019-03-02 19:20:06 -0500

    Fibonacci

--- src/math/problems/Fibonacci.java
+++ src/math/problems/Fibonacci.java
@@ -6,7 +6,13 @@ public class Fibonacci {
           Write 40 Fibonacci numbers with java.
           0,1,1,2,3,5,8,13
          */
-
+         int n =0,n1=2,count =40;
+         for (int i=0;i<count;i++){
+             int sum =n+n1;
+             n=n1;
+             n1=sum;
+             System.out.println("Fibonacci :"+sum);
+         }
 
 
     }
