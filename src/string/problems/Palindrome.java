77543cac2953f0954962cdb12a93816d62c1bbb3
sheikhraman <sheikhraman@yahoo.com>
2019-03-06 21:08:44 -0500

    Palindromes

--- src/string/problems/Palindrome.java
+++ src/string/problems/Palindrome.java
@@ -7,5 +7,27 @@ public class Palindrome {
           Palindrome. So write java code to check if a given String is Palindrome or not.
          */
 
+        String inputString ="DAD";
+        int lenght =inputString.length();
+        int i, start,end,middle;
+        start =0;
+        end =lenght -1;
+        middle =(start+end)/2;
+        for (i=start;i<=middle;i++){
+            if(inputString.charAt(start) == inputString.charAt(end)){
+                start++;
+                end--;
+            }
+            else {
+                break;
+            }
+        }
+        if(i == middle +1){
+            System.out.println("Successful");
+        }
+        else {
+            System.out.println(" Not successful");
+        }
+
     }
 }
